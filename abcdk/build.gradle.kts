8fab01d102b325d285d2ca12a634c2b626fb844f
Marek Langiewicz <marek.langiewicz@gmail.com>
2023-11-24 19:11:24 +0100

    fun

--- abcdk/build.gradle.kts
+++ abcdk/build.gradle.kts
@@ -169,7 +169,6 @@ fun TaskContainer.withPublishingPrintln() = withType<AbstractPublishToMaven>().c
     }
 }
 
-@Suppress("UNUSED_VARIABLE")
 fun Project.defaultBuildTemplateForJvmLib(
     details: LibDetails = rootExtLibDetails,
     withTestJUnit4: Boolean = false,
@@ -260,7 +259,6 @@ fun Project.defaultBuildTemplateForMppLib(
 }
 
 /** Only for very standard small libs. In most cases it's better to not use this function. */
-@Suppress("UNUSED_VARIABLE")
 fun KotlinMultiplatformExtension.allDefault(
     withJvm: Boolean = true,
     withJs: Boolean = true,
@@ -316,14 +314,14 @@ fun KotlinMultiplatformExtension.jsDefault(
 ) {
     js(IR) {
         if (withBrowser) browser {
-            testTask(Action {
+            testTask {
                 useKarma {
                     when (testWithChrome to testHeadless) {
                         true to true -> useChromeHeadless()
                         true to false -> useChrome()
                     }
                 }
-            })
+            }
         }
         if (withNode) nodejs()
     }
