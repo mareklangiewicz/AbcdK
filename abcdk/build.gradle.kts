954b79cdd573a651e041c17cf47193248f11fa0b
Marek Langiewicz <marek.langiewicz@gmail.com>
2024-07-11 11:39:18 +0200

    fun

--- abcdk/build.gradle.kts
+++ abcdk/build.gradle.kts
@@ -230,7 +230,7 @@ fun Project.defaultBuildTemplateForBasicMppLib(
       addCommonMainDependencies = addCommonMainDependencies,
     )
   }
-  configurations.checkVerSync()
+  configurations.checkVerSync(warnOnly = true)
   tasks.defaultKotlinCompileOptions(jvmTargetVer = null) // jvmVer is set below with jvmToolchain
   tasks.defaultTestsOptions(onJvmUseJUnitPlatform = details.settings.withTestJUnit5)
   if (plugins.hasPlugin("maven-publish")) {
