53b5a5f5dbb72a34cd2d8d0602ffa71c469cea27
Marek Langiewicz <marek.langiewicz@gmail.com>
2026-09-13 19:55:54 +0200

    fun

--- abcdk/build.gradle.kts
+++ abcdk/build.gradle.kts
@@ -1,9 +1,8 @@
-
 // region [[Basic MPP Lib Build Imports and Plugs]]
 
+import com.vanniktech.maven.publish.*
 import org.jetbrains.kotlin.gradle.dsl.*
 import org.jetbrains.kotlin.gradle.plugin.*
-import com.vanniktech.maven.publish.*
 import pl.mareklangiewicz.defaults.*
 import pl.mareklangiewicz.deps.*
 import pl.mareklangiewicz.utils.*
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -33,8 +33,8 @@ pluginManagement {
 }
 
 plugins {
-  id("pl.mareklangiewicz.deps.settings") version "0.4.21" // https://plugins.gradle.org/search?term=mareklangiewicz
-  id("com.gradle.develocity") version "4.5.0" // https://docs.gradle.com/develocity/gradle-plugin/
+  id("pl.mareklangiewicz.deps.settings") version "0.4.24" // https://plugins.gradle.org/search?term=mareklangiewicz
+  id("com.gradle.develocity") version "4.5.1" // https://docs.gradle.com/develocity/gradle-plugin/
 }
 
 develocity {
